b7828f4879bcf753f55ab2323c4d1d475a028c70
Paul <hhiinaarruu@gmail.com>
2018-11-02 09:39:02 +0700

    new commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 module ApplicationHelper
 
   def alert_for(flash_type)
+
     { success: 'alert-success',
       error: 'alert-danger',
       alert: 'alert-warning',
       notice: 'alert-info'
     }[flash_type.to_sym] || flash_type.to_s
+    
   end
   
 end
