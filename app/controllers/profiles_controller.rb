016b4261a46b10bf14d931acd622cbd661815a85
Paul <hhiinaarruu@gmail.com>
2018-11-02 18:04:18 +0700

    final1

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+  before_action :owned_profile, only: [:edit, :update]
+  before_action :authenticate_user!
+  before_action :set_user
   def show
     @user = User.find_by(user_name: params[:user_name])
     @posts = User.find_by(user_name: params[:user_name]).posts.order('created_at DESC')
@@ -24,6 +27,18 @@ class ProfilesController < ApplicationController
 
   def profile_params
     params.require(:user).permit(:avatar, :bio)
+  end
+
+  def owned_profile
+    @user = User.find_by(user_name: params[:user_name])
+    unless current_user == @user
+      flash[:alert] = "That profile doesn't belong to you!"
+      redirect_to root_path
+    end
+  end
+
+  def set_user
+    @user = User.find_by(user_name: params[:user_name])
   end
 
 end
