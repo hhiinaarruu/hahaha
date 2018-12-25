13f946f58dba0dd907bb7cb1bdb04413734fc022
paul <hhiinaarruu@gmail.com>
2018-12-25 09:47:14 +0700

    loadMoreComments update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,5 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery.turbolinks
+//= require jquery_ujs
+//= require loadMoreComments
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'posts#index'
 
+  namespace :admin do
+    root 'posts#index'
+    resources :posts, except: [:show]
+  end
+
   resources :posts do
     resources :comments
-    
+
     member do
       get 'like'
       get 'unlike'
@@ -15,5 +20,5 @@ Rails.application.routes.draw do
   get ':user_name', to: 'profiles#show', as: :profile
   get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
   patch ':user_name/edit', to: 'profiles#update', as: :update_profile
-  
+
 end
